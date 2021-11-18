c7195ce04ae3b343468cfe50cdaf3ce15c43eac2
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2021-11-17 19:03:20 -0500

    Update options.sol

--- options.sol
+++ options.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 
 interface EACAggregatorProxy {
     function latestAnswer() external view returns (int256);
